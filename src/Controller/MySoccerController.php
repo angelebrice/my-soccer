2d0322a0a913270d76f010e55011c5dbb46cb2b9
angelebrice <brice.ang@icloud.com>
2020-06-08 16:47:50 +0200

    mise en place du thème + nav bar

---
+++ src/Controller/MySoccerController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MySoccerController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('my_soccer/index.html.twig', [
+            'controller_name' => 'MySoccerController',
+        ]);
+    }
+}
